e894a418c41ef6451cb3d558481187bafd4db98c
Luky Rahman <lukyrahman68@gmail.com>
2018-11-13 16:43:49 +0700

    add responsive to blog

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -10,7 +10,7 @@
                     <img src="{{asset('img/home_img.jpg')}}"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="5" class="rev-slidebg" data-no-retina>
                     <!-- LAYERS -->
                     <div class="slider_text_box">
-                       
+
                         <div class="tp-caption secand_text"
                         data-x="['center','center','center','center']"
                         data-y="['middle','middle','middle','middle']"
@@ -30,7 +30,7 @@
                         data-paddingbottom="[10,10,10,10]"
                         data-paddingleft="[0,0,0,0]"><span>FULL PROJECT</span></div>
 
-                        
+
                     </div>
                 </li>
                 <li data-index="rs-2973" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="default"  data-thumb="{{asset('img/home-slider/slider-1.jpg')}}"  data-rotate="0"  data-saveperformance="off"  data-title="Web Show" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
@@ -167,11 +167,91 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-9" style="position:relative;">
-                <span style="position:absolute;max-width: 15%;,ax-height: 15%;padding: 10px;text-align: center">tes</span>
-                    <img src="{{asset('img/blog/sb.png')}}" alt="" style="max-width:100%">  
+                    <span style="position:absolute;max-width: 15%;,ax-height: 15%;padding: 10px;text-align: center">tes</span>
+                    <img src="{{asset('img/blog/sb.png')}}" alt="" style="max-width:100%">
+                    <small>Posted by Admin <span style="color: red">Rendy</span></small>
+                    <div class="col-md-12">
+                        SEMUABISA CINEMA <br>
+                        <p style="text-indent: 25px;text-align: justify">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati repellendus,
+                            dolorem adipisci sequi debitis mollitia ea laboriosam dolorum id, iusto blanditiis
+                            labore harum numquam doloremque nobis quas. Quia, magni debitis.
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati repellendus,
+                            dolorem adipisci sequi debitis mollitia ea laboriosam dolorum id, iusto blanditiis
+                            labore harum numquam doloremque nobis quas. Quia, magni debitis.
+                        </p>
+                        <span>Tag: <span style="font-style: italic;">Camera, Lensa, Hardware</span></span>
+
+                    </div>
+                    <div class="col-md-12">
+                        <div class="also" style="margin-top: 1em; margin-bottom: 1em;">
+                            <span style="font-size: 25px;font-weight: bold;">You may also like</span>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="{{asset('img/blog/lates_post.png')}}" alt="" srcset="" style="max-width:100%;"><br>
+                                <small>Camera digunakan banyak orang untuk ...</small>
+                            </div>
+                            <div class="col-md-6">
+                                <img src="{{asset('img/blog/lates_post.png')}}" alt="" srcset="" style="max-width:100%;"><br>
+                                <small>Camera digunakan banyak orang untuk ...</small>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-3" style="text-align: center">
+                                <label for="nama">Nama</label>
+                            </div>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-3" style="text-align: center">
+                                <label for="nama">Komentar</label>
+                            </div>
+                            <div class="col-md-9">
+                                <textarea name="" id="" rows="5" class="form-control"></textarea>
+                            </div>
+                        </div><br>
+                        <div class="pull-right">
+                            <input type="button" value="Post" class="btn btn-primary btn-sm">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <span style="font-size: 25px;font-weight: bold">21 Comment</span>
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="{{asset('img/blog/lates_post.png')}}" alt="" srcset="" style="max-width:100%;">
+                            </div>
+                            <div class="col-md-8 col-sm-8 col-xs-8">
+                                <label for="commentName">Rendy Destara</label>
+                                <small>May 22, 2017 AT 5:48 AM</small><br>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi omnis modi voluptatem perferendis
+                                    quo! Vero, excepturi fugiat delectus quibusdam aut ratione optio beatae est id dignissimos quae perferendis dolorem doloribus.
+                                </p>
+                                <br>
+                                <div class="pull-left">
+                                    <input type="button" value="Balas" class="btn btn-primary btn-sm">
+                                </div>
+                            </div>
+                        </div><br>
+                    </div>
                 </div>
-                <div class="col-md-3" style="border:solid">
-                    asd
+                <div class="col-md-3">
+                    <span style="font-size: 30px">LATEST POST</span><hr>
+                    <div style="margin:1em;">
+                        <img src="{{asset('img/blog/lates_post.png')}}" alt="" srcset="" style="max-width:100%;"><br>
+                        <small>Camera digunakan banyak orang untuk ...</small>
+                    </div>
+                    <div style="margin:1em;">
+                        <img src="{{asset('img/blog/lates_post.png')}}" alt="" srcset="" style="max-width:100%;"><br>
+                        <small>Camera digunakan banyak orang untuk ...</small>
+                    </div>
                 </div>
             </div>
         </div>

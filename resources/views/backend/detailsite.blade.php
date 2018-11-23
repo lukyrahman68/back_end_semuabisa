3ed00195910c03a0fcfe4508de8e2c65acec0c47
Wildan Harits <wildanharits@gmail.com>
2018-11-23 17:27:58 +0700

    minor update

---
+++ resources/views/backend/detailsite.blade.php
@@ -0,0 +1,44 @@
+
+@extends('layouts.app_backend')
+@section('content')
+
+    
+    <div class="box box-info">
+                <div class="box-header">
+                  <i class="fa fa-envelope"></i>
+    
+                  <h3 class="box-title">Detail site</h3>
+                  <!-- tools box -->
+                  <div class="pull-right box-tools">
+                    
+                  </div>
+                  <!-- /. tools -->
+                </div>
+                <div class="box-body">
+                @if (session('sukses'))
+                  <div class="alert alert-success">
+                      {{ session('sukses') }}
+                  </div>
+              @endif
+
+              @if (session('gagal'))
+                  <div class="alert alert-danger">
+                      {{ session('gagal') }}
+                  </div>
+              @endif
+                  
+                    <div class="form-group">
+                    <strong>Judul:</strong>
+                        {{ $site->nama }}
+                    </div>
+                    <div class="form-group">
+                    <strong>Kategori:</strong>
+                        {{ $site->kategori }}
+                    </div>
+                    <div>
+                    <strong>Deskripsi:</strong>
+                        {{$site->deskripsi}}   
+                    </div>
+                    <div class="box-footer clearfix">
+              </div>
+    @endsection
